7e13753aecd106ddf31c08ea7fbbb4ffad763084
DianaCuervo07 <dianamargarita07@gmail.com>
2022-06-02 14:50:51 -0500

    repaso

--- FT-M1-master/EjerciciosExtras/homework/Repaso-M1.js
+++ FT-M1-master/EjerciciosExtras/homework/Repaso-M1.js
@@ -16,9 +16,18 @@ const {
 
 var countArray = function(array) {
     // Tu código aca:
-    
+    let sum= 0;
+    for (let i = 0; i < array.length; i++) {
+       if (Array.isArray(array[i])) { 
+       sum = sum + countArray(array[i]);    
+    }else {
+        sum = sum + array[i]
+    }
 }
-
+    console.log(sum);
+    return sum
+}
+countArray([1, [2, [3, 4]], [5, 6], 7]);
 
 // Implementar la función countProps: a partir de un objeto en el cual cada propiedad puede contener
 // cualquier tipo de dato, determinar la cantidad de propiedades de objetos en cualquier nivel, ya sea el inicial
@@ -39,6 +48,14 @@ var countArray = function(array) {
 
 var countProps = function(obj) {
     // Tu código aca:
+    var count = 0;
+    for (var property in obj) {
+      count++;
+      if (typeof obj[property] === "object" && !Array.isArray(obj[property])) {
+        count = count + countProps(obj[property]);
+      }
+    }
+    return count;
 
 }
 
@@ -52,9 +69,21 @@ var countProps = function(obj) {
 //    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
 
 LinkedList.prototype.changeNotNumbers = function(){
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      if (!isNaN(current.value)) {
+        current = current.next;
+      } else {
+        current.value = "Kiricocho";
+        count++;
+        current = current.next;
+      }
+    }
+    return count;
     // Tu código aca:
-
-}
+    
+};
 
 
 // Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
@@ -67,8 +96,38 @@ LinkedList.prototype.changeNotNumbers = function(){
 
 var mergeQueues = function(queueOne, queueTwo) {
     // Tu código aca:
+    let newQueue = new Queue();
+    //   while (queueOne.size() || queueTwo.size()) {
+    //       let one = queueOne.dequeue()
+    //       let two = queueTwo.dequeue()
+    //       one && newQueue.enqueue(one)
+    // if (one){
+    //     newQueue.enqueue(one)
+    // }
+    //       two && newQueue.enqueue(two)
+    //   }
+    // console.log("queueOne", queueOne.array)
+    // console.log("queueTwo", queueTwo.array)
+    let size;
+    if (queueOne.array.length > queueTwo.array.length) {
+      size = queueOne.array.length;
+    } else {
+      size = queueTwo.array.length;
+    }
+    for (let i = 0; i < size; i++) {
+      if (queueOne.array[i]) {
+        newQueue.array.push(queueOne.array[i]);
+      }
+      if (queueTwo.array[i]) {
+        newQueue.array.push(queueTwo.array[i]);
+      }
+    }
+    // console.log(newQueue)
+    return newQueue;
+  };
+  // mergeQueues()
 
-}
+
 
 
 // Implementar la funcion closureMult que permita generar nuevas funciones que representen
@@ -82,15 +141,24 @@ var mergeQueues = function(queueOne, queueTwo) {
 
 var closureMult = function(multiplier) {
     // Tu código aca:
+    return function (num){
+      return multiplier * num;
+    };
 
-}
+};
 
 // Implementar el método sum dentro del prototype de BinarySearchTree
 // que debe retornar la suma total de los valores dentro de cada nodo del arbol
-BinarySearchTree.prototype.sum = function() {
     // Tu código aca:
+var result = 0;
+BinarySearchTree.prototype.sum = function () {
+  if (!this.left && !this.right) return this.value;
+  if (!this.left && this.right) return this.value + this.right.sum();
+  if (this.left && !this.right) return this.value + this.left.sum();
+  return this.value + this.left.sum() + this.right.sum();
+};
 
-}
+var bst = new BinarySearchTree(10);
 
 module.exports = {
     countArray,
